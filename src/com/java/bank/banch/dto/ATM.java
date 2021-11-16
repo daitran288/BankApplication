ebe30bb43a752365e284b852551bc4d143fae5e4
Dai Tran <daitran28@gmail.com>
2021-11-15 18:43:37 -0700

    Added location coord to the ATM class

--- src/com/java/bank/banch/dto/ATM.java
+++ src/com/java/bank/banch/dto/ATM.java
@@ -2,4 +2,15 @@ package com.java.bank.banch.dto;
 
 public class ATM {
 
+	private String locationCoord;
+
+	public String getLocationCoord() {
+		return locationCoord;
+	}
+
+	public void setLocationCoord(String locationCoord) {
+		this.locationCoord = locationCoord;
+	}
+	
+	
 }
