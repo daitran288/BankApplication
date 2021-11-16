2d30cc1214fb27dc808777100e9db38544a13000
Dai Tran <daitran28@gmail.com>
2021-11-15 18:33:21 -0700

    Fixed a defect on MainClass.java

--- src/com/java/bank/MainClass.java
+++ src/com/java/bank/MainClass.java
@@ -8,15 +8,14 @@ public class MainClass {
 	
 	public static void main(String[] args) {
 		Customer customer = generateCustommerInformation();
-		Customer newCustomer = CustomerService.signUp(customer);
 	}
 
 	private static Customer generateCustommerInformation() {
 		Customer customer = new Customer();
-		customer.setFirstName("Joe");
-		customer.setLastName("Billy");
+		customer.setFirstName("Jane");
+		customer.setLastName("Bob");
 		customer.setAge(31);
-		customer.setOccupation("Doctor");
+		customer.setOccupation("Janitor");
 		return customer;
 	}
 
